691c34b0d99c4979c670d01cc8ef59ba9f0435e4
Raheel Ahmed <101556343+raheel-436@users.noreply.github.com>
2024-12-26 11:32:28 +0500

    'Added: Theme customization, Font size Changing functionality'

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
 const menuItems = document.querySelectorAll(".menu-item");
+/*---Messages---*/
 const messageNotification = document.querySelector("#messages-notification");
 const messages = document.querySelector(".messages");
 const message = document.querySelectorAll(".message");
 const messageSearchBox = document.querySelector("#message-search");
+/*---Themes---*/
+const theme = document.querySelector("#theme");
+const customizeThemeBox = document.querySelector(".customize-theme");
+/*---Fonts---*/
+const fontSizes = document.querySelectorAll(".choose-size span");
+let root = document.querySelector(":root");
 
 /*-------------------------SIDEBAR------------------------------------*/
 //remove active class from all menu items
@@ -51,4 +58,60 @@ messageNotification.addEventListener("click", () => {
   setTimeout(() => {
     messages.style.boxShadow = "none";
   }, 2000);
+});
+
+/*-------------------------Theme Customization-----------------------------*/
+//open theme customization box
+const openThemeCustomization = () => {
+  customizeThemeBox.style.display = "grid";
+};
+
+theme.addEventListener("click", openThemeCustomization);
+
+//closing theme customization box
+const closeThemeCustomization = (e) => {
+  if (e.target.classList.contains("customize-theme")) {
+    customizeThemeBox.style.display = "none";
+  }
+};
+
+customizeThemeBox.addEventListener("click", closeThemeCustomization);
+
+//Font Sizes
+
+const removeSizes = () => {
+  fontSizes.forEach((size) => {
+    size.classList.remove("active");
+  });
+};
+
+fontSizes.forEach((size) => {
+  size.addEventListener("click", () => {
+    removeSizes();
+    size.classList.toggle("active");
+    let fontSize;
+
+    if (size.classList.contains("font-size-1")) {
+      fontSize = "10px";
+      root.style.setProperty("----sticky-top-left", "5.4rem");
+      root.style.setProperty("----sticky-top-right", "5.4rem");
+    } else if (size.classList.contains("font-size-2")) {
+      fontSize = "13px";
+      root.style.setProperty("----sticky-top-left", "5.4rem");
+      root.style.setProperty("----sticky-top-right", "-7rem");
+    } else if (size.classList.contains("font-size-3")) {
+      fontSize = "16px";
+      root.style.setProperty("----sticky-top-left", "-2rem");
+      root.style.setProperty("----sticky-top-right", "-17rem");
+    } else if (size.classList.contains("font-size-4")) {
+      fontSize = "19px";
+      root.style.setProperty("----sticky-top-left", "-5rem");
+      root.style.setProperty("----sticky-top-right", "-25rem");
+    } else if (size.classList.contains("font-size-5")) {
+      fontSize = "22px";
+      root.style.setProperty("----sticky-top-left", "-10rem");
+      root.style.setProperty("----sticky-top-right", "-33rem");
+    }
+    document.querySelector("html").style.fontSize = fontSize;
+  });
 });
